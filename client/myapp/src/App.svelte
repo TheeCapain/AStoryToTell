ca25a05284a09d78f3605f62ac37d1bdb4390ddf
August <Augusthauerslev@outlook.com>
2022-11-30 15:05:53 +0100

    ínitial commit

---
+++ client/myapp/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+import Home from './components/home/home.svelte'
+import Navbar from './components/navbar/navbar.svelte'
+import Sidebar from './components/SideBar/sidebar.svelte';
+</script>
+<Navbar/>
+<Sidebar/>
+
+<style>
+
+</style>
---
+++ client/myapp/src/components/SideBar/sidebar.svelte
@@ -0,0 +1,34 @@
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="side-bar">
+  <a href="/"><i class="fa fa-home"></i></a>
+  <a href="/"><i class="fa fa-search"></i></a>
+  <a href="/"><i class="fa fa-envelope"></i></a>
+  <a href="/"><i class="fa fa-globe"></i></a>
+  <a href="/"><i class="fa fa-trash"></i></a>
+</div>
+<style>
+    .side-bar {
+  width: 10%; /* Set a specific width */
+  background-color: #555; /* Dark-grey background */
+  width: 250px;
+  position: absolute;
+  top: 7%;
+  bottom: 0;
+}
+
+.side-bar a {
+  display: block; /* Make the links appear below each other instead of side-by-side */
+  text-align: center; /* Center-align text */
+  padding: 16px; /* Add some padding */
+  transition: all 0.3s ease; /* Add transition for hover effects */
+  color: white; /* White text color */
+  font-size: 36px; /* Increased font-size */
+}
+
+.side-bar a:hover {
+  background-color: #000; /* Add a hover color */
+}
+
+</style>
