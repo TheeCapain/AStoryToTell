ffc7a1d329b9d3445fef15e84b58d9a98257f33e
August <Augusthauerslev@outlook.com>
2022-12-05 16:10:43 +0100

    setup mail router but not nodemailer

---
+++ server/routers/mail/mailRouter.js
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+import nodemailer from 'nodemailer'
+
+const mailRouter = Router()
+
+mailRouter.post("/api/mails/welcome", async (req, res) => {
+    console.log(req.body.email)
+    const email = req.body.email
+
+    handleEmail()
+})
+async function handleEmail(){
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'pinkie15@ethereal.email',
+            pass: 'KDFAXpr6Rt3H1jJxuu'
+        }
+    });
+    
+        let info = await transporter.sendMail({
+            from: '"Pinkie Moen" <pinkie15@ethereal.email>',
+            to: "rhoda52@ethereal.email",
+            subject: "Welcome to the Weather",
+            text: "You are now signed up to view the weather... didnt think youd have to",
+            html: "<b>Hello world?</b>",
+          });
+    
+    
+    
+    console.log("Message sent: %s", info.messageId);
+    
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    
+    }
+
+export default mailRouter
