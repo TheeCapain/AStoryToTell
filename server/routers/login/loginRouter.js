1fb35cf402c95512feef629386334f953cab92ca
August <Augusthauerslev@outlook.com>
2022-12-05 16:08:49 +0100

    checking for changes

--- server/routers/login/loginRouter.js
+++ server/routers/login/loginRouter.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import bcrypt from 'bcrypt'
 const loginRouter = Router();
 
-loginRouter.get("/api/users/login", async (req, res, next) => {
+loginRouter.get("/api/login", async (req, res, next) => {
 
     const user = await db.get("SELECT * FROM users WHERE user_mail=?", [req.body.email])
     const istrue = bcrypt.compare(req.body.password, user.user_pw)
