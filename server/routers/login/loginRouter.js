430332df4833e94e07b5a1aca02543e2ce48d3c6
August <Augusthauerslev@outlook.com>
2022-12-05 15:57:58 +0100

    set up login and signup routers

--- server/routers/login/loginRouter.js
+++ server/routers/login/loginRouter.js
@@ -1,6 +1,19 @@
-import {Router} from 'express'
-
+import { Router } from 'express'
+import bcrypt from 'bcrypt'
 const loginRouter = Router();
 
+loginRouter.get("/api/users/login", async (req, res, next) => {
 
-export default loginRouter;
+    const user = await db.get("SELECT * FROM users WHERE user_mail=?", [req.body.email])
+    const istrue = bcrypt.compare(req.body.password, user.user_pw)
+
+    if (istrue) {
+        console.log("session is set");
+    } else {
+        console.log("test")
+        res.status(400).send({ message: "Wrong email or password" });
+    }
+
+});
+
+export default loginRouter
--- server/routers/signup/signupRouter.js
+++ server/routers/signup/signupRouter.js
@@ -1,14 +1,25 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import db from '../../database/connection_sqlite.js'
 import bcrypt from 'bcrypt'
 
 const signupRouter = Router();
+const saltRounds = 10;
+
+const salt = bcrypt.genSaltSync(saltRounds);
+
+function hashPW(pw) {
+    const hashedPW = bcrypt.hashSync(pw, salt);
+    return hashedPW
+}
 
 
+signupRouter.get("/api/signup", async (req, res) => {
+    console.log(req.body.email)
 
+    const hashedPw = hashPW(req.body.pswd)
 
-
-signupRouter.get("/api/signup",(req,res)=>{
+    console.log(hashedPw)
+    await db.run(`INSERT INTO users (user_mail, user_pw) VALUES (?, ?)`, [req.body.email, hashedPw])
 
 })
 
