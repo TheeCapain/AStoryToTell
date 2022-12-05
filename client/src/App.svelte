61c5eed8dc4775ce3a28f055f11e2108d81c3564
August <Augusthauerslev@outlook.com>
2022-12-05 16:38:47 +0100

    cannot concentrate enough to do this

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,6 @@
 <script>
     import Navigator from "./public/components/routers/navigator.svelte";
     import Sidebar from "./public/components/sidebar/sidebar.svelte";
-    import { user } from "./global/global";
-
-
-    function handleLogout() {
-        $user = null;
-    }
 </script>
 
 <Navigator />
